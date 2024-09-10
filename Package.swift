72f1ff778ae2701c1a96f90579b9ae571d69f5b7
Rudrank Riyam <rudrankriyam@gmail.com>
2024-09-10 08:29:07 +0530

    Add visionOS support

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
          .iOS(.v15),
          .macOS(.v13),
+         .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
