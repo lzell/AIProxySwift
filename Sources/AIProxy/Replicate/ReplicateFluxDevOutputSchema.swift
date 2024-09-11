174fe7e6fc0071a040c8a517c9943b5d6c77d9f6
Lou Zell <lzell11@gmail.com>
2024-09-11 10:57:43 -0700

    Make Flux replicate type aliases public (#34)
    Fix a regression in 0.16.0 which caused existing client code to throw compilation error:
    
    ```
    Generic parameter 'U' could not be inferred
    Cannot find type 'ReplicateFluxDevOutputSchemas' in scope
    ```

--- Sources/AIProxy/Replicate/ReplicateFluxDevOutputSchema.swift
+++ Sources/AIProxy/Replicate/ReplicateFluxDevOutputSchema.swift
@@ -9,4 +9,4 @@ import Foundation
 
 /// Output schema for use with requests to Black Forest Lab's Flux Dev model:
 /// https://replicate.com/black-forest-labs/flux-dev/api/schema#output-schema
-typealias ReplicateFluxDevOutputSchema = [URL]
+public typealias ReplicateFluxDevOutputSchema = [URL]
