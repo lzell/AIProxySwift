2f93a32c35b20bd55412d486b5aeeeb4c9c426aa
Daniel Aditya Istyana <daniel.istyana@bytedance.com>
2024-10-10 01:13:28 +0700

    update file name, move to extension and make some property optional

--- Sources/AIProxy/OpenAI/OpenAITextToSpeechRequestBody.swift
+++ Sources/AIProxy/OpenAI/OpenAITextToSpeechRequestBody.swift
@@ -1,6 +1,5 @@
 //
-//  
-//  OpenAITextToSpeechCodables.swift
+//  OpenAITextToSpeechRequestBody.swift
 //
 //
 //  Created by Daniel Aditya Istyana on 10/9/24.
@@ -9,6 +8,8 @@
 
 import Foundation
 
+/// Docstrings from 
+/// https://platform.openai.com/docs/api-reference/audio/createSpeech
 public struct OpenAITextToSpeechRequestBody: Encodable {
 
     // Required
@@ -24,35 +25,44 @@ public struct OpenAITextToSpeechRequestBody: Encodable {
     public let voice: Voice
 
     /// The format to audio in. Supported formats are `mp3`, `opus`, `aac`, `flac`, `wav`, and `pcm`.
-    public let responseFormat: ResponseFormat
+    public let responseFormat: ResponseFormat?
 
     /// The speed of the generated audio. Select a value from 0.25 to 4.0. 1.0 is the default.
-    public let speed: Float
-    
-    public enum Voice: String, Encodable {
-        case alloy, echo, fable, onyx, nova, shimmer
-    }
-    
-    public enum ResponseFormat: String, Encodable {
-        case mp3, opus, aac, flac, wav, pcm
-    }
-    
-    public enum Model: String, Encodable {
-        case tts1 = "tts-1"
-        case tts1HD = "tts-1-hd"
-    }
-    
+    public let speed: Float?
+
     public init(
         input: String,
         model: Model = .tts1,
         voice: OpenAITextToSpeechRequestBody.Voice,
-        responseFormat: OpenAITextToSpeechRequestBody.ResponseFormat = .mp3,
-        speed: Float = 1.0
+        responseFormat: OpenAITextToSpeechRequestBody.ResponseFormat? = .mp3,
+        speed: Float? = 1.0
     ) {
         self.input = input
         self.model = model
         self.voice = voice
         self.responseFormat = responseFormat
         self.speed = speed
+    }
+}
+
+// MARK: - OpenAITextToSpeechRequestBody.Model
+public extension OpenAITextToSpeechRequestBody {
+    enum Model: String, Encodable {
+        case tts1 = "tts-1"
+        case tts1HD = "tts-1-hd"
+    }
+}
+
+// MARK: - OpenAITextToSpeechRequestBody.ResponseFormat
+public extension OpenAITextToSpeechRequestBody {
+    enum ResponseFormat: String, Encodable {
+        case mp3, opus, aac, flac, wav, pcm
+    }
+}
+
+// MARK: - OpenAITextToSpeechRequestBody.Voice
+public extension OpenAITextToSpeechRequestBody {
+    enum Voice: String, Encodable {
+        case alloy, echo, fable, onyx, nova, shimmer
     }
 }
