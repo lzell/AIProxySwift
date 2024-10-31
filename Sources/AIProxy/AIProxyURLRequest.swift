78b2000fa08f635e71611db7ee4529ac431d6c6b
Martin Wright <martin.wright@evergreenapps.co.uk>
2024-10-31 10:30:10 +0000

    Additional public declarations #60

--- Sources/AIProxy/AIProxyURLRequest.swift
+++ Sources/AIProxy/AIProxyURLRequest.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct AIProxyURLRequest {
+public struct AIProxyURLRequest {
 
     /// Creates a URLRequest that is configured for use with an AIProxy URLSession.
     static func create(
