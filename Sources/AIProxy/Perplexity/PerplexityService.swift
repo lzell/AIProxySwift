16ecf1217d365a24072d5f4f456a6557c168f07a
Lou Zell <lzell11@gmail.com>
2024-11-06 12:35:10 -0800

    Add support for Perplexity.ai (#70)

---
+++ Sources/AIProxy/Perplexity/PerplexityService.swift
@@ -0,0 +1,191 @@
+//
+//  PerplexityService.swift
+//
+//
+//  Created by Lou Zell on 11/06/24.
+//
+
+import Foundation
+
+open class PerplexityService {
+    private let partialKey: String
+    private let serviceURL: String
+    private let clientID: String?
+
+    /// Creates an instance of PerplexityService. Note that the initializer is not public.
+    /// Customers are expected to use the factory `AIProxy.perplexityService` defined in AIProxy.swift
+    internal init(
+        partialKey: String,
+        serviceURL: String,
+        clientID: String?
+    ) {
+        self.partialKey = partialKey
+        self.serviceURL = serviceURL
+        self.clientID = clientID
+    }
+
+    /// Initiates a non-streaming chat completion request to Perplexity
+    ///
+    /// - Parameters:
+    ///   - body: The chat completion request body. See this reference:
+    ///   https://platform.openai.com/docs/api-reference/chat/object
+    ///
+    /// - Returns: A ChatCompletionResponse
+    public func chatCompletionRequest(
+        body: PerplexityChatCompletionRequestBody
+    ) async throws -> PerplexityChatCompletionResponseBody {
+        var body = body
+        body.stream = false
+        let session = AIProxyURLSession.create()
+        let request = try await AIProxyURLRequest.create(
+            partialKey: self.partialKey,
+            serviceURL: self.serviceURL,
+            clientID: self.clientID,
+            proxyPath: "/chat/completions",
+            body:  try body.serialize(),
+            verb: .post,
+            contentType: "application/json"
+        )
+
+        let (data, res) = try await session.data(for: request)
+        guard let httpResponse = res as? HTTPURLResponse else {
+            throw AIProxyError.assertion("Network response is not an http response")
+        }
+
+        if (httpResponse.statusCode > 299) {
+            throw AIProxyError.unsuccessfulRequest(
+                statusCode: httpResponse.statusCode,
+                responseBody: String(data: data, encoding: .utf8) ?? ""
+            )
+        }
+
+        return try PerplexityChatCompletionResponseBody.deserialize(from: data)
+    }
+
+    /// Initiates a streaming chat completion request to Perplexity.
+    ///
+    /// - Parameters:
+    ///
+    ///   - body: The chat completion request body. See this reference:
+    ///   https://platform.openai.com/docs/api-reference/chat/object
+    ///
+    /// - Returns: An async sequence of completion chunks.
+    public func streamingChatCompletionRequest(
+        body: PerplexityChatCompletionRequestBody
+    ) async throws -> AsyncCompactMapSequence<AsyncLineSequence<URLSession.AsyncBytes>, PerplexityChatCompletionResponseBody> {
+        var body = body
+        body.stream = true
+        let session = AIProxyURLSession.create()
+        let request = try await AIProxyURLRequest.create(
+            partialKey: self.partialKey,
+            serviceURL: self.serviceURL,
+            clientID: self.clientID,
+            proxyPath: "/chat/completions",
+            body:  try body.serialize(),
+            verb: .post,
+            contentType: "application/json"
+        )
+
+        let (asyncBytes, res) = try await session.bytes(for: request)
+
+        guard let httpResponse = res as? HTTPURLResponse else {
+            throw AIProxyError.assertion("Network response is not an http response")
+        }
+
+        if (httpResponse.statusCode > 299) {
+            let responseBody = try await asyncBytes.lines.reduce(into: "") { $0 += $1 }
+            throw AIProxyError.unsuccessfulRequest(
+                statusCode: httpResponse.statusCode,
+                responseBody: responseBody
+            )
+        }
+
+        return asyncBytes.lines.compactMap { PerplexityChatCompletionResponseBody.from(line: $0) }
+    }
+
+
+//    /// Initiates a streaming chat completion request to Groq.
+//    ///
+//    /// - Parameters:
+//    ///   - body: The chat completion request body. See this reference:
+//    ///           https://console.groq.com/docs/api-reference#chat-create
+//    /// - Returns: An async sequence of completion chunks. See this reference:
+//    ///            https://platform.openai.com/docs/api-reference/chat/streaming
+//    public func streamingChatCompletionRequest(
+//        body: GroqChatCompletionRequestBody
+//    ) async throws -> AsyncCompactMapSequence<AsyncLineSequence<URLSession.AsyncBytes>, GroqChatCompletionStreamingChunk> {
+//        var body = body
+//        body.stream = true
+//        let session = AIProxyURLSession.create()
+//        let request = try await AIProxyURLRequest.create(
+//            partialKey: self.partialKey,
+//            serviceURL: self.serviceURL,
+//            clientID: self.clientID,
+//            proxyPath: "/openai/v1/chat/completions",
+//            body:  try body.serialize(),
+//            verb: .post,
+//            contentType: "application/json"
+//        )
+//
+//        let (asyncBytes, res) = try await session.bytes(for: request)
+//
+//        guard let httpResponse = res as? HTTPURLResponse else {
+//            throw AIProxyError.assertion("Network response is not an http response")
+//        }
+//
+//        if (httpResponse.statusCode > 299) {
+//            let responseBody = try await asyncBytes.lines.reduce(into: "") { $0 += $1 }
+//            throw AIProxyError.unsuccessfulRequest(
+//                statusCode: httpResponse.statusCode,
+//                responseBody: responseBody
+//            )
+//        }
+//
+//        return asyncBytes.lines.compactMap { GroqChatCompletionStreamingChunk.from(line: $0) }
+//    }
+//
+//    /// Initiates a transcription request to /openai/v1/audio/transcriptions
+//    ///
+//    /// - Parameters:
+//    ///   - body: The audio transcription request body. See this reference:
+//    ///           https://console.groq.com/docs/api-reference#audio-transcription
+//    /// - Returns: An transcription response. See this reference:
+//    ///            https://platform.openai.com/docs/api-reference/audio/json-object
+//    public func createTranscriptionRequest(
+//        body: GroqTranscriptionRequestBody
+//    ) async throws -> GroqTranscriptionResponseBody {
+//        let session = AIProxyURLSession.create()
+//        let boundary = UUID().uuidString
+//        let request = try await AIProxyURLRequest.create(
+//            partialKey: self.partialKey,
+//            serviceURL: self.serviceURL,
+//            clientID: self.clientID,
+//            proxyPath: "/openai/v1/audio/transcriptions",
+//            body: formEncode(body, boundary),
+//            verb: .post,
+//            contentType: "multipart/form-data; boundary=\(boundary)"
+//        )
+//
+//        let (data, res) = try await session.data(for: request)
+//        guard let httpResponse = res as? HTTPURLResponse else {
+//            throw AIProxyError.assertion("Network response is not an http response")
+//        }
+//
+//        if (httpResponse.statusCode > 299) {
+//            throw AIProxyError.unsuccessfulRequest(
+//                statusCode: httpResponse.statusCode,
+//                responseBody: String(data: data, encoding: .utf8) ?? ""
+//            )
+//        }
+//
+//        if (body.responseFormat == "text") {
+//            guard let text = String(data: data, encoding: .utf8) else {
+//                throw AIProxyError.assertion("Could not represent OpenAI's whisper response as string")
+//            }
+//            return GroqTranscriptionResponseBody(duration: nil, language: nil, segments: nil, text: text, words: nil)
+//        }
+//
+//        return try GroqTranscriptionResponseBody.deserialize(from: data)
+//    }
+
+}
