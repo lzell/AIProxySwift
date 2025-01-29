2e9f6880b7f2618757054dd54403a189786f77dd
Lou Zell <lzell11@gmail.com>
2025-01-29 00:10:39 -0800

    Add a receipt validation service

---
+++ Sources/AIProxy/ReceiptValidation/ReceiptValidationService.swift
@@ -0,0 +1,43 @@
+//
+//  ReceiptValidationService.swift
+//  AIProxy
+//
+//  Created by Lou Zell on 1/28/25.
+//
+
+open class ReceiptValidationService: ProxiedService {
+
+    public let publishableKey: String
+    public let serviceURL: String
+    public let clientID: String?
+
+    public init(
+        publishableKey: String,
+        serviceURL: String,
+        clientID: String? = nil
+    ) {
+        self.publishableKey = publishableKey
+        self.serviceURL = serviceURL
+        self.clientID = clientID
+    }
+
+    /// Makes a request to validate the app purchase
+    ///
+    /// - Parameters:
+    ///   - body: The request body to send to aiproxy
+    /// - Returns: The validation result
+    public func validateReceipt(
+        body: ReceiptValidationRequestBody
+    ) async throws -> ReceiptValidationResponseBody {
+        let request = try await AIProxyURLRequest.create(
+            partialKey: self.publishableKey,
+            serviceURL: self.serviceURL,
+            clientID: self.clientID,
+            proxyPath: "/validate",
+            body: try body.serialize(),
+            verb: .post,
+            contentType: "application/json"
+        )
+        return try await self.makeRequestAndDeserializeResponse(request)
+    }
+}
