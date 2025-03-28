7c9f4b78cd1d7312e17330ad219c7cd9c21eb36a
Lou Zell <lzell11@gmail.com>
2025-03-27 23:27:39 -0700

    Bump sdkVersion

--- Sources/AIProxy/AIProxy.swift
+++ Sources/AIProxy/AIProxy.swift
@@ -8,7 +8,7 @@ import UIKit
 public struct AIProxy {
 
     /// The current sdk version
-    public static let sdkVersion = "0.80.0"
+    public static let sdkVersion = "0.81.0"
 
     /// - Parameters:
     ///   - partialKey: Your partial key is displayed in the AIProxy dashboard when you submit your provider's key.
