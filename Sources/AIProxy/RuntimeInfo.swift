1280711b63d5a584a044167d0b356263378d740b
Lou Zell <lzell11@gmail.com>
2025-05-21 14:23:11 -0700

    Don't use Host name, it could contain PII

--- Sources/AIProxy/RuntimeInfo.swift
+++ Sources/AIProxy/RuntimeInfo.swift
@@ -23,27 +23,47 @@ struct RuntimeInfo {
     static var current: RuntimeInfo = {
         let bundle = Bundle.main
         let infoDict = bundle.infoDictionary ?? [:]
-        var deviceModel = "Unknown"
-        var systemName = "Unknown"
-        let osVersion = ProcessInfo.processInfo.operatingSystemVersion
-        let osVersionString = "\(osVersion.majorVersion).\(osVersion.minorVersion).\(osVersion.patchVersion)"
-
-        #if os(iOS) || os(tvOS) || os(watchOS)
-        deviceModel = UIDevice.current.model
-        systemName = UIDevice.current.systemName
-        #elseif os(macOS)
-        deviceModel = Host.current().localizedName ?? "Mac"
-        systemName = "macOS"
-        #endif
 
         return RuntimeInfo(
             appName: infoDict["CFBundleName"] as? String ?? "Unknown",
             appVersion: infoDict["CFBundleShortVersionString"] as? String ?? "Unknown",
             buildNumber: infoDict["CFBundleVersion"] as? String ?? "Unknown",
             bundleID: bundle.bundleIdentifier ?? "Unknown",
-            deviceModel: deviceModel,
-            osVersion: osVersionString,
-            systemName: systemName
+            deviceModel: getDeviceModel(),
+            osVersion: getOSVersion(),
+            systemName: getSystemName()
         )
     }()
+}
+
+private func getDeviceModel() -> String {
+    #if os(macOS)
+    let sysCallName = "hw.model"
+    #else
+    let sysCallName = "hw.machine"
+    #endif
+    var size: size_t = 0
+    guard sysctlbyname(sysCallName, nil, &size, nil, 0) == noErr, size > 0 else {
+        return "Unknown"
+    }
+
+    var machine = [CChar](repeating: 0, count: size)
+    guard sysctlbyname(sysCallName, &machine, &size, nil, 0) == noErr else {
+        return "Unknown"
+    }
+
+    return String(cString: machine)
+}
+
+private func getSystemName() -> String {
+    #if os(macOS)
+    return "macOS"
+    #else
+    return UIDevice.current.systemName
+    #endif
+}
+
+private func getOSVersion() -> String {
+    let osVersion = ProcessInfo.processInfo.operatingSystemVersion
+    return "\(osVersion.majorVersion).\(osVersion.minorVersion).\(osVersion.patchVersion)"
 }
