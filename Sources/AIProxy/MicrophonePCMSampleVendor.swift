76033f3f0d03581b89a6d2e4844d6b7e9ef9323f
Lou Zell <lzell11@gmail.com>
2025-05-27 23:38:12 -0700

    Getting mic data

--- Sources/AIProxy/MicrophonePCMSampleVendor.swift
+++ Sources/AIProxy/MicrophonePCMSampleVendor.swift
@@ -6,6 +6,194 @@
 //
 
 // This file is not compatible with watchOS
+#if os(watchOS)
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The class for handling AVAudioEngine.
+*/
+
+import AVFoundation
+import Foundation
+import CoreAudio
+
+/// This is an AVAudioEngine-based implementation that vends PCM16 microphone samples.
+///
+/// ## Requirements
+///
+/// - Assumes an `NSMicrophoneUsageDescription` description has been added to Target > Info
+/// - Assumes that microphone permissions have already been granted
+///
+/// ## Usage
+///
+/// ```
+///     let microphoneVendor = try MicrophonePCMSampleVendor()
+///     microphoneVendor.start { sample in
+///        // Do something with `sample`
+///        // Note: this callback is invoked on a background thread
+///     }
+/// ```
+///
+///
+/// References:
+/// Apple sample code: https://developer.apple.com/documentation/avfaudio/using-voice-processing
+/// Apple technical note: https://developer.apple.com/documentation/technotes/tn3136-avaudioconverter-performing-sample-rate-conversions
+/// My apple forum question: https://developer.apple.com/forums/thread/771530
+@RealtimeActor
+open class MicrophonePCMSampleVendor {
+    private var avAudioEngine: AVAudioEngine?
+    private var inputNode: AVAudioInputNode?
+    private var continuation: AsyncStream<AVAudioPCMBuffer>.Continuation?
+    private var audioConverter: AVAudioConverter?
+
+
+    
+    public init() {}
+    deinit {
+        logIf(.debug)?.debug("MicrophonePCMSampleVendor is being freed")
+    }
+
+
+    public func start() throws -> AsyncStream<AVAudioPCMBuffer> {
+        let avAudioEngine = AVAudioEngine()
+        let inputNode = avAudioEngine.inputNode
+        print(inputNode.inputFormat(forBus: 0))
+        // Only conditionally enable this if my headphones are not applied. Bizarrely, this causes the mic on my headphones to not work at all.
+        // Wait am I sure I want this at all? This really dips the output volume, which I need if the AI is talking
+        //
+        // It's also possible that just by using audio engine our problems will be solved.
+        // try inputNode.setVoiceProcessingEnabled(true)
+        // Important! Do not try to use inputNode.inputFormat(forBus: 0) after enabling setVoiceProcessingEnabled.
+        // Turning on voice processing changes the mic input format from a single channel to five channels, and
+        // those five channels do not play nicely with AVAudioConverter.
+        // Instead, specify a desired format on the input tap and let AudioEngine deal with the conversion itself.
+
+        // And do I want this?
+        // self.inputNode!.isVoiceProcessingBypassed = false
+
+        guard let desiredTapFormat = AVAudioFormat(
+            commonFormat: .pcmFormatInt16,
+            sampleRate: inputNode.inputFormat(forBus: 0).sampleRate,
+            channels: 1,
+            interleaved: false
+        ) else {
+            throw AIProxyError.assertion("Could not create the desired tap format for realtime")
+        }
+
+        // The buffer size argument specifies the target number of audio frames.
+        // For a single channel, a single audio frame has a single audio sample.
+        // So we are shooting for 1 sample every 100 ms with this calulation.
+        //
+        // There is a note on the installTap documentation that says AudioEngine may
+        // adjust the bufferSize internally.
+        let targetBufferSize = UInt32(desiredTapFormat.sampleRate / 10)
+        // var firstSampleThrownOut = false
+        print("Target buffer size is \(targetBufferSize)")
+        let stream = AsyncStream<AVAudioPCMBuffer> { [weak self] continuation in
+            print("lzell in stream block")
+            guard let this = self else { return }
+            print("lzell setting continuation")
+            this.continuation = continuation
+            inputNode.installTap(onBus: 0, bufferSize: targetBufferSize, format: desiredTapFormat) { [weak this] sampleBuffer, _ in
+                if let resampledBuffer = self?.convertPCM16BufferToExpectedSampleRate(sampleBuffer) {
+                    print("Got sample buf")
+                    this?.continuation?.yield(resampledBuffer)
+                }
+            }
+        }
+
+        avAudioEngine.prepare()
+        try avAudioEngine.start()
+        self.avAudioEngine = avAudioEngine
+        self.inputNode = inputNode
+        return stream
+    }
+
+    func stop() {
+        self.continuation?.finish()
+        self.continuation = nil
+        print("Removing the tap")
+        inputNode!.removeTap(onBus: 0)
+        try? inputNode!.setVoiceProcessingEnabled(false)
+        inputNode = nil
+        avAudioEngine!.stop()
+        avAudioEngine = nil
+        audioConverter = nil
+    }
+
+    // THIS IS EXACTLY THE SAME AS THE IMPLEMENTATION BELOW. DRY.
+    private func convertPCM16BufferToExpectedSampleRate(_ pcm16Buffer: AVAudioPCMBuffer) -> AVAudioPCMBuffer? {
+        // if ll(.debug) { aiproxyLogger.debug("Captured \(pcm16Buffer.frameLength) pcm16 samples from the mic") }
+        guard let audioFormat = AVAudioFormat(
+            commonFormat: .pcmFormatInt16,
+            sampleRate: 24000.0,
+            channels: 1,
+            interleaved: false
+        ) else {
+            logIf(.error)?.error("Could not create target audio format")
+            return nil
+        }
+
+        if self.audioConverter == nil {
+            self.audioConverter = AVAudioConverter(from: pcm16Buffer.format, to: audioFormat)
+        }
+
+        guard let converter = self.audioConverter else {
+            logIf(.error)?.error("There is no audio converter to use for PCM16 resampling")
+            return nil
+        }
+
+        guard let outputBuffer = AVAudioPCMBuffer(
+            pcmFormat: audioFormat,
+            frameCapacity: AVAudioFrameCount(audioFormat.sampleRate * 2.0)
+        ) else {
+            logIf(.error)?.error("Could not create output buffer for PCM16 resampling")
+            return nil
+        }
+
+#if false
+        writePCM16IntValuesToFile(from: pcm16Buffer, location: "output1.txt")
+#endif
+
+        // See the docstring on AVAudioConverterInputBlock in AVAudioConverter.h
+        //
+        // The block will keep getting invoked until either the frame capacity is
+        // reached or outStatus.pointee is set to `.noDataNow` or `.endStream`.
+        var error: NSError?
+        var ptr: UInt32 = 0
+        let targetFrameLength = pcm16Buffer.frameLength
+        let _ = converter.convert(to: outputBuffer, error: &error) { numberOfFrames, outStatus in
+            guard ptr < targetFrameLength,
+                  let workingCopy = advancedPCMBuffer_noCopy(pcm16Buffer, offset: ptr)
+            else {
+                outStatus.pointee = .noDataNow
+                return nil
+            }
+            let amountToFill = min(numberOfFrames, targetFrameLength - ptr)
+            outStatus.pointee = .haveData
+            ptr += amountToFill
+            workingCopy.frameLength = amountToFill
+            return workingCopy
+        }
+
+        if let error = error {
+            logIf(.error)?.error("Error converting to expected sample rate: \(error.localizedDescription)")
+            return nil
+        }
+
+#if false
+        writePCM16IntValuesToFile(from: outputBuffer, location: "output2.txt")
+#endif
+
+        return outputBuffer
+    }
+
+}
+
+
+#endif
+
 #if !os(watchOS)
 import AVFoundation
 import AudioToolbox
@@ -370,6 +558,29 @@ open class MicrophonePCMSampleVendor {
     }
 }
 
+
+@RealtimeActor
+private let audioRenderCallback: AURenderCallback = {
+    inRefCon,
+    ioActionFlags,
+    inTimeStamp,
+    inBusNumber,
+    inNumberFrames,
+    ioData in
+    let microphonePCMSampleVendor = Unmanaged<MicrophonePCMSampleVendor>
+        .fromOpaque(inRefCon)
+        .takeUnretainedValue()
+    microphonePCMSampleVendor.didReceiveRenderCallback(
+        ioActionFlags,
+        inTimeStamp,
+        inBusNumber,
+        inNumberFrames
+    )
+    return noErr
+}
+#endif
+
+
 private func advancedPCMBuffer_noCopy(_ originalBuffer: AVAudioPCMBuffer, offset: UInt32) -> AVAudioPCMBuffer? {
     let audioBufferList = originalBuffer.mutableAudioBufferList
     guard audioBufferList.pointee.mNumberBuffers == 1,
@@ -418,24 +629,3 @@ private func writePCM16IntValuesToFile(from buffer: AVAudioPCMBuffer, location: 
         }
     }
 }
-
-@RealtimeActor
-private let audioRenderCallback: AURenderCallback = {
-    inRefCon,
-    ioActionFlags,
-    inTimeStamp,
-    inBusNumber,
-    inNumberFrames,
-    ioData in
-    let microphonePCMSampleVendor = Unmanaged<MicrophonePCMSampleVendor>
-        .fromOpaque(inRefCon)
-        .takeUnretainedValue()
-    microphonePCMSampleVendor.didReceiveRenderCallback(
-        ioActionFlags,
-        inTimeStamp,
-        inBusNumber,
-        inNumberFrames
-    )
-    return noErr
-}
-#endif
