92fe813f0f09f620bec7693aa9223f2e15b4f480
Lou Zell <lzell11@gmail.com>
2025-08-12 10:50:08 -0700

    Building but tests failing

--- Sources/AIProxy/OpenAI/OpenAIResponseInput.swift
+++ Sources/AIProxy/OpenAI/OpenAIResponseInput.swift
@@ -5,6 +5,8 @@
 //  Created by Lou Zell on 8/12/25.
 //
 
+import Foundation
+
 extension OpenAIResponse {
     public enum Input: Encodable {
         case text(String)
@@ -20,57 +22,9 @@ extension OpenAIResponse {
             }
         }
     }
+}
 
-    public enum Role: String, Encodable {
-        case user
-        case assistant
-        case system
-        case developer
-    }
-
-    public enum Content: Encodable {
-        case text(String)
-        case list([ItemContent])
-
-        public func encode(to encoder: any Encoder) throws {
-            var container = encoder.singleValueContainer()
-            switch self {
-            case .text(let txt):
-                try container.encode(txt)
-            case .list(let itemContent):
-                try container.encode(itemContent)
-            }
-        }
-    }
-
-    public enum ItemContent: Encodable {
-        case file(fileID: String)
-        case imageURL(URL)
-        case text(String)
-
-        private enum CodingKeys: String, CodingKey {
-            case fileID = "file_id"
-            case imageURL = "image_url"
-            case type
-            case text
-        }
-
-        public func encode(to encoder: any Encoder) throws {
-            var container = encoder.container(keyedBy: CodingKeys.self)
-            switch self {
-            case .file(let fileID):
-                try container.encode(fileID, forKey: .fileID)
-                try container.encode("input_file", forKey: .type)
-            case .imageURL(let imageURL):
-                try container.encode(imageURL, forKey: .imageURL)
-                try container.encode("input_image", forKey: .type)
-            case .text(let txt):
-                try container.encode(txt, forKey: .text)
-                try container.encode("input_text", forKey: .type)
-            }
-        }
-    }
-
+extension OpenAIResponse.Input {
     public enum ItemOrMessage: Encodable {
         case message(role: Role, content: Content)
 
@@ -97,6 +51,60 @@ extension OpenAIResponse {
             switch self {
             case .message(role: let role, content: let content):
                 try container.encode(_Message(role: role, content: content))
+            }
+        }
+    }
+}
+
+extension OpenAIResponse.Input.ItemOrMessage {
+    public enum Role: String, Encodable {
+        case user
+        case assistant
+        case system
+        case developer
+    }
+
+    public enum Content: Encodable {
+        case text(String)
+        case list([ItemContent])
+
+        public func encode(to encoder: any Encoder) throws {
+            var container = encoder.singleValueContainer()
+            switch self {
+            case .text(let txt):
+                try container.encode(txt)
+            case .list(let itemContent):
+                try container.encode(itemContent)
+            }
+        }
+    }
+}
+
+extension OpenAIResponse.Input.ItemOrMessage.Content {
+    public enum ItemContent: Encodable {
+        case file(fileID: String)
+        case imageURL(URL)
+        case text(String)
+
+        private enum CodingKeys: String, CodingKey {
+            case fileID = "file_id"
+            case imageURL = "image_url"
+            case type
+            case text
+        }
+
+        public func encode(to encoder: any Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            switch self {
+            case .file(let fileID):
+                try container.encode(fileID, forKey: .fileID)
+                try container.encode("input_file", forKey: .type)
+            case .imageURL(let imageURL):
+                try container.encode(imageURL, forKey: .imageURL)
+                try container.encode("input_image", forKey: .type)
+            case .text(let txt):
+                try container.encode(txt, forKey: .text)
+                try container.encode("input_text", forKey: .type)
             }
         }
     }
