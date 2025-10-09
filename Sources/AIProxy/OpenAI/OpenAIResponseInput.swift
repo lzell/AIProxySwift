9db52ed06876c4bbee8deb6900a54a4301084e64
hungrxyz <6236080+hungrxyz@users.noreply.github.com>
2025-10-09 18:40:29 +0200

    removes commented out code

--- Sources/AIProxy/OpenAI/OpenAIResponseInput.swift
+++ Sources/AIProxy/OpenAI/OpenAIResponseInput.swift
@@ -180,18 +180,6 @@ extension OpenAIResponse.Input {
             
             let id: String
             
-//            /// Constrains effort on reasoning for [reasoning models](https://platform.openai.com/docs/guides/reasoning).
-//            /// Currently supported values are low, medium, and high. Reducing reasoning effort can result in faster responses and fewer tokens used on reasoning in a response.
-//            let effort: Effort?
-//
-//            /// A summary of the reasoning performed by the model. This can be useful for debugging and understanding the model's reasoning process.
-//            let generateSummary: String?
-
-//            private enum CodingKeys: String, CodingKey {
-//                case id
-//                case effort
-//                case generateSummary = "generate_summary"
-//            }
         }
 
         public func encode(to encoder: any Encoder) throws {
