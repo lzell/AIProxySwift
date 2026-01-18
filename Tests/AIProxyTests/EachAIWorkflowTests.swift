50289010388529aee06fa9e065b9199935869d13
Furkan Simsir <furkansimsir@icloud.com>
2026-01-18 03:09:47 +0100

    Fix EachAI decoding error and add `parameters` value in response (#237)

--- Tests/AIProxyTests/EachAIWorkflowTests.swift
+++ Tests/AIProxyTests/EachAIWorkflowTests.swift
@@ -32,7 +32,6 @@ final class EachAIWorkflowTests: XCTestCase {
           "average_percent": 100,
           "flow_name": "1940's - Clone",
           "organization_id": "1049",
-          "api_key": "MP1019YB1TSIBUCMOWNQL3V54NY2CC6GZGXM",
           "execution_id": "mp53sjtsdgl83y2wm",
           "source_ip_address": "",
           "parameters": [
@@ -94,6 +93,103 @@ final class EachAIWorkflowTests: XCTestCase {
         XCTAssertEqual("Face Swap", res.stepResults?.last?.stepName)
     }
 
+    // See https://github.com/lzell/AIProxySwift/pull/237
+    func testFlowExecutionResponseWithMixedFormatsForStepOutputIsDecodable() throws {
+        let sampleResponse = #"""
+        {
+          "flow_id": "bda5f7e3-d146-454d-bdb6-befb47adb2b1",
+          "average_percent": 100,
+          "flow_name": "1940's - Clone",
+          "organization_id": "1049",
+          "execution_id": "mpttjxmg407ul9z3f",
+          "source_ip_address": "",
+          "parameters": [
+            {
+              "name": "img",
+              "value": "https://storage.googleapis.com/magicpoint/models/women.png"
+            }
+          ],
+          "step_results": [
+            {
+              "step_id": "step1",
+              "step_name": "Face Analyzer",
+              "model": "1019-face-analyzer",
+              "version": "0.0.1",
+              "started_at": "2026-01-18T01:46:09Z",
+              "ended_at": "2026-01-18T01:46:14Z",
+              "status": "succeeded",
+              "output": "{\"age\":\"25\",\"eye_color\":\"brown\",\"gender\":\"female\",\"hair_color\":\"brown\",\"race\":\"white\",\"skin_color\":\"light\"}",
+              "output_json": [
+                {
+                  "Key": "race",
+                  "Value": "white"
+                },
+                {
+                  "Key": "skin_color",
+                  "Value": "light"
+                },
+                {
+                  "Key": "age",
+                  "Value": "25"
+                },
+                {
+                  "Key": "eye_color",
+                  "Value": "brown"
+                },
+                {
+                  "Key": "gender",
+                  "Value": "female"
+                },
+                {
+                  "Key": "hair_color",
+                  "Value": "brown"
+                }
+              ],
+              "input": "{\"image_url\":\"https://storage.googleapis.com/magicpoint/models/women.png\"}"
+            },
+            {
+              "step_id": "step2",
+              "step_name": "Image Generation",
+              "model": "photomaker",
+              "version": "0.0.1",
+              "started_at": "2026-01-18T01:46:24Z",
+              "ended_at": "2026-01-18T01:46:30Z",
+              "status": "succeeded",
+              "output": "\"https://storage.googleapis.com/1019uploads/da9f520e-1465-4ac7-9b80-64ff0f9cd0ee.png\"",
+              "output_json": [
+                "https://storage.googleapis.com/1019uploads/da9f520e-1465-4ac7-9b80-64ff0f9cd0ee.png"
+              ],
+              "input": "{\"guidance_scale\":5,\"input_image\":\"https://storage.googleapis.com/magicpoint/models/women.png\",\"negative_prompt\":\"nsfw, lowres, bad anatomy, hand, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry\",\"num_outputs\":1,\"num_steps\":20,\"prompt\":\" Vintage 1920s portrait of a flapper 25-year-old white {{step1.output.gender} img with a bob haircut, captured with a Graflex Speed Graphic camera, f/4.5 lens, 1/50 sec exposure, black and white, grainy, soft focus.\",\"style_name\":\"Photographic (Default)\",\"style_strength_ratio\":20}"
+            },
+            {
+              "step_id": "step3",
+              "step_name": "Face Swap",
+              "model": "face-swap-new",
+              "version": "0.0.1",
+              "started_at": "2026-01-18T01:46:41Z",
+              "ended_at": "2026-01-18T01:46:55Z",
+              "status": "succeeded",
+              "output": "\"https://storage.googleapis.com/1019uploads/30ba32df-207e-4337-90c2-708369a1d5cc.jpg\"",
+              "output_json": "https://storage.googleapis.com/1019uploads/30ba32df-207e-4337-90c2-708369a1d5cc.jpg",
+              "input": "{\"input_image\":\"https://storage.googleapis.com/1019uploads/da9f520e-1465-4ac7-9b80-64ff0f9cd0ee.png\",\"swap_image\":\"https://storage.googleapis.com/magicpoint/models/women.png\"}"
+            }
+          ],
+          "status": "succeeded",
+          "output": "\"https://storage.googleapis.com/1019uploads/30ba32df-207e-4337-90c2-708369a1d5cc.jpg\"",
+          "output_json": "https://storage.googleapis.com/1019uploads/30ba32df-207e-4337-90c2-708369a1d5cc.jpg",
+          "created_at": "2026-01-18T01:46:03Z",
+          "started_at": "2026-01-18T01:46:03Z",
+          "ended_at": "2026-01-18T01:46:55Z",
+          "updated_at": "",
+          "deleted_at": "",
+          "average_response_time": 0,
+          "cost": null
+        }
+        """#
+        let res = try EachAIWorkflowExecutionResponseBody.deserialize(from: sampleResponse)
+        XCTAssertEqual("succeeded", res.status)
+    }
+
     func testPendingFlowExecutionResponseIsDecodable() throws {
         let sampleResponse = #"""
         {
@@ -101,7 +197,6 @@ final class EachAIWorkflowTests: XCTestCase {
           "average_percent": 9.816666666666666,
           "flow_name": "1940's - Clone",
           "organization_id": "1049",
-          "api_key": "MP1019YB1TSIBUCMOWNQL3V54NY2CC6GZGXM",
           "execution_id": "mpzzednstsokliyjz",
           "source_ip_address": "",
           "parameters": [
@@ -161,4 +256,3 @@ final class EachAIWorkflowTests: XCTestCase {
         let _ = try EachAIWorkflowExecutionResponseBody.deserialize(from: sampleResponse)
     }
 }
-
