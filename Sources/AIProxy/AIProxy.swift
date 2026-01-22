15f8fcd186dec278eda07d88101abaefbf178f90
Lou Zell <lzell11@gmail.com>
2026-01-22 10:07:04 -0800

    Bump version number

--- Sources/AIProxy/AIProxy.swift
+++ Sources/AIProxy/AIProxy.swift
@@ -8,7 +8,7 @@ import UIKit
 public enum AIProxy {
 
     /// The current sdk version
-    nonisolated public static let sdkVersion = "0.143.0"
+    nonisolated public static let sdkVersion = "0.144.0"
 
     /// Configures the AIProxy SDK. Call this during app launch by adding an `init` to your SwiftUI MyApp.swift file, e.g.
     ///
